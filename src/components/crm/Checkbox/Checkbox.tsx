7a8f89b9ff3ddb42a1e29dd8c55462fda2c414a7
Nurmuhammad02 <nurmuhammad.rn@gmail.com>
2024-05-27 13:21:52 +0500

    checkbox completed for crm

--- src/components/crm/Checkbox/Checkbox.tsx
+++ src/components/crm/Checkbox/Checkbox.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import './Checkbox.css';
 
 type CheckboxSize = 'small' | 'medium' | 'large';
-type CheckboxColor =
-    'blue' | 'gray' | 'green' | 'violet'
+type CheckboxColor = 'blue' | 'gray' | 'green' | 'violet'
 
 type CheckboxPropsType = {
-    size: CheckboxSize,
-    color: CheckboxColor,
+    size: CheckboxSize
+    color: CheckboxColor
+    active: boolean
 };
 
-const Checkbox: React.FC<CheckboxPropsType> = ({size ,color, ...rest}) => {
+const Checkbox: React.FC<CheckboxPropsType> = ({size, color, active = true, ...rest}) => {
     return (
-        <input type="checkbox" className={`checkbox ${size} ${color}`} {...rest} />
+        <input type="checkbox" className={`checkbox ${size} ${color} ${active ? 'active' : ''}`} {...rest} />
     );
 };
 
